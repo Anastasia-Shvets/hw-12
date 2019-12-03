04c66ca0c5b75ef06c68d2b33431aff1480f9ab1
Anastasia <shvets.anastasia04@gmail.com>
2019-12-04 01:42:19 +0200

    hw

---
+++ script.js
@@ -0,0 +1,38 @@
+var circleMover = move(document.querySelector('.circle'));
+
+function move(el) {
+    return {
+        el: el,
+        x: 0,
+        move: function (direction, scale) {
+            var transform;
+            switch (direction) {
+                case 'decrease':
+                    transform = 'scale(' + (this.x = scale) + ')';
+                    break;
+                case 'increase':
+                    transform = 'scale( '+ (this.x = scale) + 'px, ' + (this.y -= scale) + 'px)';
+                    break;
+            }
+            this.el.style.transform = transform;
+        },
+        decrease: function (scale) {
+            this.move('decrease', scale);
+        },
+        increase: function (scale) {
+            this.move('increase', scale);
+        },
+        scale(value) {
+            transform = 'scale(' + value + ')';
+            this.el.style.transform = transform;
+        }
+    }
+}
+
+setTimeout(function () {
+    circleMover.scale(2) 
+    setTimeout(function() {
+        circleMover.scale(1) 
+    }, 500)
+}, 500)
+
