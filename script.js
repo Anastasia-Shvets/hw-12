fa69666b25f78814be0c7b90f2f06c6492d2edd5
Anastasia <shvets.anastasia04@gmail.com>
2020-01-25 21:29:18 +0200

    without Move

--- script.js
+++ script.js
@@ -4,18 +4,6 @@ function move(el) {
     return {
         el: el,
         x: 0,
-        move: function (direction, scale) {
-            var transform;
-            switch (direction) {
-                case 'decrease':
-                    transform = 'scale(' + (this.x = scale) + ')';
-                    break;
-                case 'increase':
-                    transform = 'scale( '+ (this.x = scale) + 'px, ' + (this.y -= scale) + 'px)';
-                    break;
-            }
-            this.el.style.transform = transform;
-        },
         scale(value) {
             transform = 'scale(' + value + ')';
             this.el.style.transform = transform;
